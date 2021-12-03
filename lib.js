707653d4bb921df594eadd4b7d07ded91d1253b7
Salem <owenpnstockdale@gmail.com>
2021-12-03 14:44:27 -0700

    added bow (no shooting yet), fixed enemy jittering if off its grid

--- lib.js
+++ lib.js
@@ -18,7 +18,7 @@ var player = {
     h: 50,
     xSpeed: 0,
     ySpeed: 0,
-    speed: 3,
+    speed: 5,
     currentFrame: 0,
     currentDir: 0,
     animX: 0,
@@ -26,7 +26,29 @@ var player = {
     hp: 100
 }
 
+var enemy = {
+    x: 500,
+    y: 200,
+    w: 50,
+    h: 50,
+    hp: 100,
+    damage: 10,
+    speed: 2
+}
+
 var sword = {
+    x: 375,
+    y: 520,
+    w: 50,
+    h: 50,
+    animX:  8,
+    currentDir: 0,
+    damage: 35,
+    canAttack: true,
+    attacked: false
+}
+
+var bow = {
     x: 375,
     y: 520,
     w: 50,
@@ -38,14 +60,15 @@ var sword = {
     attacked: false
 }
 
-var enemy = {
-    x: 500,
-    y: 200,
+var arrow = {
+    x: 375,
+    y: 520,
     w: 50,
     h: 50,
-    hp: 100,
-    damage: 10
+    animX: 8,
+    currentDir: 0
 }
+
 
 
 var knightSheet = document.getElementById("knightSheet");
@@ -58,12 +81,17 @@ knightSheet.onload = function() {
 }
 
 var swordImg = document.getElementById("sword");
+var bowImg = document.getElementById("bow");
+var arrowImg = document.getElementById("arrow");
 
 
 var stage = {
     x: 0,
     y: 0
 }
+
+var slot1 = document.getElementById("slot1");
+var slot2 = document.getElementById("slot2");
 
 
 // animation loop
@@ -74,19 +102,30 @@ function animate() {
     player.x += player.xSpeed;
     player.y += player.ySpeed;
 
+    if (player.y < -50) {
+        player.y = 600;
+    } else if (player.y > 600) {
+        player.y = -50;
+    } else if (player.x < -50) {
+        player.x = 800;
+    } else if (player.x > 800) {
+        player.x = -50;
+    }
+
+    
 
     // move enemy to player
     if (enemy.hp > 0) {
-        if (enemy.x < player.x) {
-            enemy.x++;
-        } else if (enemy.x > player.x) {
-            enemy.x--;
+        if (enemy.x < player.x && (player.x - enemy.x) > enemy.speed) {
+            enemy.x+=enemy.speed;
+        } else if (enemy.x > player.x && (enemy.x - player.x) > enemy.speed) {
+            enemy.x-=enemy.speed;
         }
     
-        if (enemy.y < player.y) {
-            enemy.y++;
-        } else if (enemy.y > player.y) {
-            enemy.y--;
+        if (enemy.y < player.y && (player.y - enemy.y) > enemy.speed) {
+            enemy.y+=enemy.speed;
+        } else if (enemy.y > player.y && (enemy.y - player.y) > enemy.speed) {
+            enemy.y-=enemy.speed;
         } 
     } else if (enemy.hp == 0 && enemy.x < 1000000) {
         enemy.x = 1000000;
@@ -97,9 +136,6 @@ function animate() {
     if (hitChar) {
         characterHit();
     }
-    
-
-    
 
     // move sword to player position
     if (sword.currentDir == 0) {
@@ -116,6 +152,20 @@ function animate() {
         sword.y = player.y;
     } 
 
+    if (bow.currentDir == 0) {
+        bow.x = player.x;
+        bow.y = player.y-44;
+    } else if (bow.currentDir == 1) {
+        bow.x = player.x+44;
+        bow.y = player.y;
+    } else if (bow.currentDir == 2) {
+        bow.x = player.x;
+        bow.y = player.y+44;
+    } else if (bow.currentDir == 3) {
+        bow.x = player.x-44;
+        bow.y = player.y;
+    } 
+
     if (stage.x == 0 && stage.y == 0) {
         ctx.clearRect(0, 0, cnv.width, cnv.height);
 
@@ -127,8 +177,11 @@ function animate() {
     ctx.drawImage(knightSheet, player.animX, 0, 8, 8, player.x, player.y, player.w, player.h);
 
     // draw sword
-    if (player.attacking) {
+    if (player.attacking && slot1.classList.contains("active")) {
         ctx.drawImage(swordImg, sword.animX, 0, 8, 8, sword.x, sword.y, sword.w, sword.h);
+    } else if (player.attacking && slot2.classList.contains("active")) {
+        ctx.drawImage(bowImg, bow.animX, 0, 8, 8, bow.x, bow.y, bow.w, bow.h);
+        ctx.drawImage(arrowImg, arrow.animX, 0, 8, 8, arrow.x, arrow.y, arrow.w, arrow.h)
     }
 
     // draw enemy
@@ -163,14 +216,24 @@ function keydownHandler(event) {
             player.currentDir = 1;
             walkFrame();
         } else if (event.key == "ArrowUp") {
-            attack(0, wea);
+            attack(0);
         } else if (event.key == "ArrowLeft") {
-            attack(3, wea);
+            attack(3);
         } else if (event.key == "ArrowDown") {
-            attack(2, wea);
+            attack(2);
         } else if (event.key == "ArrowRight") {
-            attack(1, wea);
-        } 
+            attack(1);
+        } else if (event.key == "1") {
+            if (!slot1.classList.contains("active")) {
+                slot1.classList.add("active");
+                slot2.classList.remove("active");
+            }
+        } else if (event.key == "2") {
+            if (!slot2.classList.contains("active")) {
+                slot2.classList.add("active");
+                slot1.classList.remove("active");
+            }
+        }
     }
 }
 
@@ -203,7 +266,7 @@ function walkFrame() {
 }
 
 function attack(dir, weapon) {
-    if (sword.canAttack) {
+    if (slot1.classList.contains("active") && sword.canAttack) {
         sword.currentDir = dir;
 
         swordAnim();
@@ -247,12 +310,22 @@ function attack(dir, weapon) {
         setTimeout(function () {
             sword.canAttack = true
         }, 400)
-    }
-}
+    } else if (slot2.classList.contains("active")) {
+        bow.currentDir = dir;
+        arrow.currentDir = dir;
 
+        bowAnim();
+        player.attacking = true;
+    }
+    
+}
 
 function swordAnim() {
     sword.animX = sword.currentDir * 8;
+}
+
+function bowAnim() {
+    bow.animX = bow.currentDir * 8;
 }
 
 function hitDetect(e1, e2) {
@@ -278,7 +351,7 @@ function characterHit() {
             console.log("player hp is now " + player.hp);
             updateHP();
             hitting = false;
-        }, 400)
+        }, 500)
     }
 }
 
